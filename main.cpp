fd0076c532100ced242253e5de9420c8e095b38a
stepan.rutz <stepan.rutz@gmx.de>
2024-12-14 08:54:57 +0100

    nested layouts

--- main.cpp
+++ main.cpp
@@ -1,25 +1,35 @@
 #include <QApplication>
 #include <QWidget>
 #include <QLabel>
+#include <QPushButton>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv); QWidget window;
     window.setWindowTitle("Layouts"); window.setGeometry(100, 100, 500, 300);
 
-    QVBoxLayout *layout = new QVBoxLayout(&window);
-    QLabel *label1 = new QLabel("Label 1");
+    auto mainLayout = new QVBoxLayout(&window);
+
+    auto topRow = new QWidget(&window);
+    auto topRowLayout = new QHBoxLayout(topRow);
+    auto button1 = new QPushButton("Button 1");
+    auto button2 = new QPushButton("Button 1");
+    auto button3 = new QPushButton("Button 1");
+    topRowLayout->addWidget(button1);
+    topRowLayout->addWidget(button2);
+    topRowLayout->addWidget(button3);
+
+    auto label1 = new QLabel("Label 1");
     label1->setStyleSheet("color: white; padding: 4px; background-color: red;");
-    QLabel *label2 = new QLabel("Label 2");
+    label1->setAlignment(Qt::AlignCenter);
+    auto label2 = new QLabel("Label 2");
     label2->setStyleSheet("color: white; padding: 4px; background-color: green;");
-    QLabel *label3 = new QLabel("Label 3");
-    label3->setStyleSheet("color: white; padding: 4px; background-color: blue;");
-    layout->setSpacing(0);
-    layout->addStretch();
-    layout->addWidget(label1, 0, Qt::AlignLeft);
-    layout->addWidget(label2, 0, Qt::AlignHCenter);
-    layout->addWidget(label3, 0, Qt::AlignRight);
-    layout->addStretch();
+    label2->setAlignment(Qt::AlignCenter);
+    mainLayout->setSpacing(0);
+    mainLayout->addWidget(topRow);
+    mainLayout->addWidget(label1, 1, Qt::Alignment());
+    mainLayout->addWidget(label2, 1, Qt::Alignment());
 
     window.show(); return app.exec();
 }
