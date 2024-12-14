ef6248dd0a60bd1f978169618cdc7c37890966e0
stepan.rutz <stepan.rutz@gmx.de>
2024-12-14 16:46:03 +0100

    sql example

--- main.cpp
+++ main.cpp
@@ -1,19 +1,50 @@
 #include <QApplication>
 #include <QWidget>
-#include <QLabel>
 #include <QVBoxLayout>
+#include <QPlainTextEdit>
+#include <QStringBuilder>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlError>
+#include <cstdlib>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
     QWidget window;
-    window.setWindowTitle("Layouts Beispiel");
-    window.move(100, 100);
-
+    window.setWindowTitle("Sql Beispiel");
+    window.setGeometry(100, 100, 600, 500);
     auto layout = new QVBoxLayout(&window);
-    auto label1 = new QLabel("Label 1");
-    layout->addWidget(label1);
+    auto output = new QPlainTextEdit();
+    output->setReadOnly(true);
+    layout->addWidget(output);
+
+    auto db = QSqlDatabase::addDatabase("QPSQL"); // QPSQL für PostgreSQL
+    db.setHostName("localhost");
+    db.setPort(5432);
+    db.setDatabaseName("template1");
+    db.setUserName("sr");
+    db.setPassword("test");
+    if (!db.open()) {
+        qDebug() << "Failed to connect to the database:" << db.lastError().text();
+        return EXIT_FAILURE;
+    }
+
+    QString s;
+    QSqlQuery query;
+    QString sql = R"(SELECT table_schema, table_name FROM information_schema.tables order by 1, 2)";
+    if (!query.exec(sql)) {
+        qDebug() << "Failed to execute query:" << query.lastError().text();
+        return EXIT_FAILURE;
+    }
+    while (query.next()) {
+        auto tableSchema = query.value("table_schema").toString();
+        auto tableName = query.value("table_name").toString();
+        s += tableSchema + "." + tableName + "\r\n";
+    }
+    db.close();
+    output->setPlainText(s);
+
     window.show();
     return app.exec();
 }
