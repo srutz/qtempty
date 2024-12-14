b065e01830796bb3118cd2f63b16daa9cab82d3c
stepan.rutz <stepan.rutz@gmx.de>
2024-12-14 09:28:17 +0100

    gridlayout

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 #include <QApplication>
 #include <QWidget>
 #include <QLabel>
-#include <QVBoxLayout>
+#include <QString>
+#include <QGridLayout>
+#include <memory>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    auto window = make_unique<QWidget>();  // QWidget* als smart-pointer
+    window->setWindowTitle("Layouts Beispiel");
+    window->move(100, 100);
 
-    QWidget window;
-    window.setWindowTitle("Layouts Beispiel");
-    window.move(100, 100);
-
-    auto layout = new QVBoxLayout(&window);
-    auto label1 = new QLabel("Label 1");
-    layout->addWidget(label1);
-    window.show();
+    const auto PER_ROW = 8;
+    auto layout = new QGridLayout(window.get());
+    for (int c = '0', i = 0; c <= 'Z'; c++, i++) {
+        auto text = QString("%1").arg(QChar((c)));
+        auto label = new QLabel(text, window.get());
+        label->setAlignment(Qt::AlignCenter);
+        label->setStyleSheet("background-color: #f1f1f1; font-size: 64px;");
+        label->setContentsMargins(QMargins(16, 16, 16, 16));
+        auto row = i / PER_ROW;
+        auto column = i % PER_ROW;
+        layout->addWidget(label, row, column);
+    }
+    window->show();
     return app.exec();
 }
 
