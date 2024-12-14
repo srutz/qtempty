f4d8ebe4dd9e866c5abcebd3dfe4eedc76b7d9a5
stepan.rutz <stepan.rutz@gmx.de>
2024-12-14 08:43:12 +0100

    alignments

--- main.cpp
+++ main.cpp
@@ -3,25 +3,24 @@
 #include <QLabel>
 #include <QVBoxLayout>
 
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
-
-    QWidget window;
-    window.setWindowTitle("Layouts Beispiel");
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv); QWidget window;
+    window.setWindowTitle("Layouts"); window.setGeometry(100, 100, 500, 300);
 
     QVBoxLayout *layout = new QVBoxLayout(&window);
-
     QLabel *label1 = new QLabel("Label 1");
+    label1->setStyleSheet("color: white; padding: 4px; background-color: red;");
     QLabel *label2 = new QLabel("Label 2");
+    label2->setStyleSheet("color: white; padding: 4px; background-color: green;");
     QLabel *label3 = new QLabel("Label 3");
+    label3->setStyleSheet("color: white; padding: 4px; background-color: blue;");
+    layout->setSpacing(0);
+    layout->addStretch();
+    layout->addWidget(label1, 0, Qt::AlignLeft);
+    layout->addWidget(label2, 0, Qt::AlignHCenter);
+    layout->addWidget(label3, 0, Qt::AlignRight);
+    layout->addStretch();
 
-    layout->addWidget(label1);
-    layout->addWidget(label2);
-    layout->addWidget(label3);
-
-    window.show();
-
-    return app.exec();
+    window.show(); return app.exec();
 }
 
