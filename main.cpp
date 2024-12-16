1b67f222753feed9b91c36e75aa51a40c66e57c9
stepan.rutz <stepan.rutz@gmx.de>
2024-12-16 20:16:09 +0100

    pdfview

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <QApplication>
 #include <QWidget>
-#include <QLabel>
 #include <QVBoxLayout>
+#include <QDir>
+#include <QStandardPaths>
+#include <QPdfView>
+#include <QPdfDocument>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
+    QObject::connect(&app, &QApplication::lastWindowClosed, [] { QApplication::quit(); });
     QWidget window;
-    window.setWindowTitle("Layouts Beispiel");
-    window.move(100, 100);
-
+    window.setWindowTitle("PdfViewer Beispiel");
+    window.setGeometry(100, 100, 900, 600);
     auto layout = new QVBoxLayout(&window);
-    auto label1 = new QLabel("Label 1");
-    layout->addWidget(label1);
+    QString filePath;
+    if (argc == 2) {
+        filePath = QString(argv[1]);
+    } else {
+        QString documentsPath = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+        QDir documentsDir(documentsPath);
+        filePath = documentsDir.filePath("cv.pdf");
+    }
+    auto viewer = new QPdfView(&window);
+    auto *document = new QPdfDocument(&window);
+    document->load(filePath);
+    viewer->setDocument(document);
+    layout->setContentsMargins(QMargins(0, 0, 0, 0));
+    layout->addWidget(viewer);
     window.show();
     return app.exec();
 }
