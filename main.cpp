4d4822532817a8121e9a903725f322e53ac09d50
stepan.rutz <stepan.rutz@gmx.de>
2025-02-08 14:27:42 +0100

    input

--- main.cpp
+++ main.cpp
@@ -2,18 +2,35 @@
 #include <QWidget>
 #include <QLabel>
 #include <QVBoxLayout>
+#include <QLineEdit>
+#include <QDateEdit>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
     QWidget window;
-    window.setWindowTitle("Layouts Beispiel");
-    window.move(100, 100);
-
+    window.resize(400, 150); window.move(100, 100);
     auto layout = new QVBoxLayout(&window);
-    auto label1 = new QLabel("Label 1");
-    layout->addWidget(label1);
+
+    // Text input
+    auto textInput = new QLineEdit(&window);
+    textInput->setPlaceholderText("Enter text here");
+    layout->addWidget(textInput);
+    QObject::connect(textInput, &QLineEdit::textChanged, &window, [textInput] {
+        qDebug() << "textchanged=" << textInput->text();
+    });
+    // Masked input for IBAN
+    auto maskedInput = new QLineEdit(&window);
+    maskedInput->setInputMask("AA99 9999 9999 9999 9999 9999 999");
+    maskedInput->setPlaceholderText("Enter IBAN");
+    layout->addWidget(maskedInput);
+    // Date input
+    auto dateInput = new QDateEdit(&window);
+    dateInput->setDisplayFormat("yyyy-MM-dd");
+    layout->addWidget(dateInput);
+    layout->addStretch(1);
+
     window.show();
     return app.exec();
 }
