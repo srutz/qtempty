61be8a404832c1235964ddc4dfe3b31036f3689a
stepan.rutz <stepan.rutz@gmx.de>
2025-02-11 12:43:44 +0100

    abstract models

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,25 +15,6 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     auto layout = new QVBoxLayout(this->centralWidget());
-    const int N_ROWS = 1000;
-    auto model = new QStandardItemModel(N_ROWS, 3, this);
-    model->setHorizontalHeaderLabels({"Number", "Square", "Delta Previous Square"});
-    for (auto i = 0; i < N_ROWS; ++i) {
-        auto number = i + 1;
-        auto square = number * number;
-        auto previousSquare = (number - 1) * (number - 1);
-        auto item1 = new QStandardItem(QString::number(number));
-        item1->setTextAlignment(Qt::AlignRight | Qt::AlignVCenter);
-        auto item2 = new QStandardItem(QString::number(square));
-        item2->setTextAlignment(Qt::AlignRight | Qt::AlignVCenter);
-        auto item3 = new QStandardItem(QString::number(square - previousSquare));
-        item3->setTextAlignment(Qt::AlignRight | Qt::AlignVCenter);
-
-        model->setItem(i, 0, item1);
-        model->setItem(i, 1, item2);
-        model->setItem(i, 2, item3);
-    }
-
     auto tableView = new QTableView(this);
     auto mymodel = new MyModel(this);
     mymodel->addPerson({ .email = "frank@gmx.de", .age = 20 });
