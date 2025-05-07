e86c4e6376954b6fa41d447c3c09130e129dc855
stepan.rutz <stepan.rutz@gmx.de>
2025-05-07 15:25:00 +0200

    sql querymodel

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@
 #include <QHeaderView>
 #include <QAbstractTableModel>
 #include <QString>
-
-#include "model.h"
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlQueryModel>
+#include <utility>
 
 int main(int argc, char *argv[])
 {
@@ -15,8 +18,22 @@ int main(int argc, char *argv[])
     window.resize(300, 200);
     window.move(100, 100);
 
+    auto db = QSqlDatabase::addDatabase("QPSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("template1");
+    db.setUserName("sr");
+    db.setPassword("test99");
+    if (!db.open()) {
+        qDebug() << "connect error" << db.lastError(); QApplication::exit(1);
+    }
+    QSqlQuery q;
+    q.prepare("select 100 * 10 as tausend, table_schema, table_name, table_type from information_schema.tables order by 1");
+    if (!q.exec()) {
+        qDebug() << "query error" << q.lastError(); QApplication::exit(1);
+    }
     auto layout = new QVBoxLayout(&window);
-    auto model = new CustomTableModel(&window);
+    auto model = new QSqlQueryModel(&window);
+    model->setQuery(std::move(q));
 
     auto tableView = new QTableView(&window);
     tableView->setModel(model);
